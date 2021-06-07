ac6d7844a2681d6ba48bd28367da56a8fca987b9
Dumitru <ru.dumit@gmail.com>
2021-06-07 16:07:03 +0200

    test finished

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+function capitalizeFirstLetters(input) {
+    if (input === "") return "";
+    return input.split(" ").map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+
+// Check that capitalizeFirstLetters is a function
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+
+// Check that capitalizeFirstLetters accepts one argument
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+
+// Check that capitalizeFirstLetters transforms i am learning TDD correctly
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+
+// Check that it works with one word 
+assert.strictEqual(capitalizeFirstLetters('data'), 'Data');
+
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+
+// Check that it works for an empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
